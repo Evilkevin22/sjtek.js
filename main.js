f2985e5f0424a451f677a247996402beff3fc8aa
Kevin Grond <kevingrond@gmail.com>
2017-04-03 21:55:55 +0200

    Dynamic question loading

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
+var tellie = -1;
+var app = new Vue({
+    el: '.container',
+    data: {
+        question: "yolo",
+        explanation: "diek",
+        firstImage: "",
+        firstButton: "asdad",
+        secondImage: "",
+        secondButton: "dgdssss"
+    }
+});
+
 (function init() {
+ next();
+})();
 
-    var app = new Vue({
-        el: '.container',
-        data: {
-            question: "yolo",
-            explanation: "diek",
-            firstImage: "",
-            firstButton: "asdad",
-            secondImage: "",
-            secondButton: "dgdssss"
-        }
-    });
 
+function next() {
+
+    tellie++;
     var request = new Request('./data/questions.json', {
         method: 'GET',
     });
@@ -19,14 +26,13 @@
     fetch(request).then(function (response) {
 
         response.json().then(function (json) {
-            console.log("JSON:" + json[0].question);
+            console.log("JSON:" + json[tellie].question);
 
 
-            app.question = json[0].question;
+            app.question = json[tellie].question;
             console.log("Data:" + app.question);
 
 
         });
     });
-})();
-
+}
